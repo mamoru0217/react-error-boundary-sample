392119d9351d2cf2780f380e1079de4728790cd7
Mamoru Amano <booktower103@gmail.com>
2018-01-15 19:48:00 +0900

    First commit

---
+++ index.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class Main extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      count: 0
+    }
+
+    this.onClickButton = this.onClickButton.bind(this)
+    this.throwError = this.throwError.bind(this)
+  }
+
+  onClickButton() {
+    this.setState({
+      count: ++this.state.count
+    })
+  }
+
+  throwError() {
+    if (this.state.count > 5) {
+      throw new Error('Do not touch me!')
+    }
+  }
+
+  render() {
+    this.throwError()
+    return (
+      <div>
+        <p>{this.state.count}</p>
+        <button onClick={this.onClickButton}>Increment</button>
+      </div>
+    )
+  }
+}
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  componentDidCatch(error, info) {
+    this.setState({ hasError: true });
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Something went wrong.</h1>;
+    }
+    return this.props.children
+  }
+}
+
+ReactDOM.render(
+  <ErrorBoundary>
+    <Main/>
+  </ErrorBoundary>,
+  document.querySelector('#main')
+)
